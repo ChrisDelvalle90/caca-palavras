fd4b1b6a7afe1a649b3f9f9624269d9a4729ba0b
Christian Delvalle <christian_mskt@hotmail.com>
2022-07-19 14:01:12 -0400

    Criacao de dois arquivos

---
+++ script.js
@@ -0,0 +1,17 @@
+function procurar()
+{
+    let frase = document.getElementById("frase").value
+    let busca = document.getElementById("busca").value
+
+    let resultado = frase.includes(busca)
+    let divResultado = document.querySelector(".resultado")
+
+    if(resultado == true)
+    {
+        divResultado.innerHTML = `<p>Palavra ${busca} encontrada</p>`
+    }
+    else 
+    {
+        divResultado.innerHTML = `<p>Palavra ${busca} não encontrada</p>`
+    }
+}
